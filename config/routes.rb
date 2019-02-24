a46ee77d5e7fafb194c02ec8f2159d1f771ef7cc
linda <ywl94@qq.com>
2019-02-24 11:40:22 +0800

    link to

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "static_pages/home" => "static_pages#home"
-  get "help"=> "static_pages#help"
+  root "static_pages#home"
+
+  get "help" => "static_pages#help"
+
+  get "about" => "static_pages#about"
+
+  get "contact" => "static_pages#contact"
+
 end
